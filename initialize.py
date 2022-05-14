ce5ffa255d256bf9b306ba811ddad8a7ef6da763
Jiang Haotian <jhht1996@gmail.com>
2022-05-14 12:42:44 +0000

    put import and initialisation  into seperate file

---
+++ initialize.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+from lib.model import Module
+from lib.utils import Dataset,train_model, inference
+from math import exp
+from lib.generator import Shift, LorenzRandFGenerator
+
+
+SEED = 1234
+DTYPE = torch.float64
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.benchmark=True
